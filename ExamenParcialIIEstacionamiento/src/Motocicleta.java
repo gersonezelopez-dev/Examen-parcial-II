7671c3043c195c2a85cd88a30aa98266a6689d79
Gerson Ezequiel López Enriquez <gersonezelopez@gmail.com>
2026-09-19 19:00:19 -0600

    Motocicleta java
    Acá trabajamos con objetos.
    
    Herencia y sobreescritura en objetos java.

--- ExamenParcialIIEstacionamiento/src/Motocicleta.java
+++ ExamenParcialIIEstacionamiento/src/Motocicleta.java
@@ -1,2 +1,22 @@
-public class Motocicleta {
+public class Motocicleta extends Vehiculo {
+
+    private static final double TARIFA = 6.00;
+
+    public Motocicleta(String placa, String propietario,
+                       String horaIngreso, double horasUtilizadas) {
+
+        super(placa, propietario, horaIngreso, horasUtilizadas);
+    }
+
+    @Override
+    public double calcularCosto() {
+
+        double costo = getHorasUtilizadas() * TARIFA;
+
+        if (getHorasUtilizadas() > 5) {
+            costo = costo * 0.90;
+        }
+
+        return costo;
+    }
 }
