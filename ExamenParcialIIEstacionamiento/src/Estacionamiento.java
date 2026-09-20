35e5fdd6fa99372dfba9a28430aa15ad1f6db2f6
Gerson Ezequiel López Enriquez <gersonezelopez@gmail.com>
2026-09-19 19:04:16 -0600

    Registrar vehículos y evitar placas duplicadas
    Evitar placas repetidas.

--- ExamenParcialIIEstacionamiento/src/Estacionamiento.java
+++ ExamenParcialIIEstacionamiento/src/Estacionamiento.java
@@ -12,4 +12,21 @@ public class Estacionamiento {
         vehiculos = new ArrayList<>();
         placas = new HashSet<>();
     }
+    public boolean registrarVehiculo(Vehiculo vehiculo) {
+
+        String placa = vehiculo.getPlaca().toUpperCase();
+
+        if (placas.contains(placa)) {
+
+            System.out.println("La placa ya se encuentra registrada.");
+            return false;
+        }
+
+        vehiculos.add(vehiculo);
+        placas.add(placa);
+
+        System.out.println("Vehículo registrado correctamente.");
+
+        return true;
+    }
 }
