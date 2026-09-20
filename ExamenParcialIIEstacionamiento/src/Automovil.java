00e5268d74fa69cf6c84008be53139e10160d7d4
Gerson Ezequiel López Enriquez <gersonezelopez@gmail.com>
2026-09-19 18:56:39 -0600

    Código Automóvil en java
    Creación de código java.
    
    Ejemplo:
    
    6 horas × Q10 = Q60
    
    Como pasó de 5 horas:
    
    Q60 - 10 % = Q54

--- ExamenParcialIIEstacionamiento/src/Automovil.java
+++ ExamenParcialIIEstacionamiento/src/Automovil.java
@@ -1,2 +1,22 @@
-public class Automovil {
+public class Automovil extends Vehiculo {
+
+    private static final double TARIFA = 10.00;
+
+    public Automovil(String placa, String propietario,
+                     String horaIngreso, double horasUtilizadas) {
+
+        super(placa, propietario, horaIngreso, horasUtilizadas);
+    }
+
+    @Override
+    public double calcularCosto() {
+
+        double costo = getHorasUtilizadas() * TARIFA;
+
+        if (getHorasUtilizadas() > 5) {
+            costo = costo * 0.90;
+        }
+
+        return costo;
+    }
 }
