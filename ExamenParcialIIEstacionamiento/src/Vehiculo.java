456ee206471b0784dc7edb4028743441e88e9ccb
Gerson Ezequiel López Enriquez <gersonezelopez@gmail.com>
2026-09-19 18:54:51 -0600

    Vehículo java
    Código Vehículo.
    
    Aquí ya estás demostrando:
    
    abstracción;
    encapsulamiento,
    constructor,
    getters,
    método abstracto,
    método para mostrar información.

--- ExamenParcialIIEstacionamiento/src/Vehiculo.java
+++ ExamenParcialIIEstacionamiento/src/Vehiculo.java
@@ -1,2 +1,42 @@
-public class Vehiculo {
+public abstract class Vehiculo {
+
+    private String placa;
+    private String propietario;
+    private String horaIngreso;
+    private double horasUtilizadas;
+
+    public Vehiculo(String placa, String propietario,
+                    String horaIngreso, double horasUtilizadas) {
+
+        this.placa = placa;
+        this.propietario = propietario;
+        this.horaIngreso = horaIngreso;
+        this.horasUtilizadas = horasUtilizadas;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public String getHoraIngreso() {
+        return horaIngreso;
+    }
+
+    public double getHorasUtilizadas() {
+        return horasUtilizadas;
+    }
+
+    public abstract double calcularCosto();
+
+    public void mostrarInformacion() {
+
+        System.out.println("Placa: " + placa);
+        System.out.println("Propietario: " + propietario);
+        System.out.println("Hora de ingreso: " + horaIngreso);
+        System.out.println("Horas utilizadas: " + horasUtilizadas);
+    }
 }
