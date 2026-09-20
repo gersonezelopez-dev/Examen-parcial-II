4349a1791d8f1ba860595e473ce0a2ccca94612a
Gerson Ezequiel López Enriquez <gersonezelopez@gmail.com>
2026-09-19 19:03:35 -0600

    Estacionamiento java
    Se registro algo como polimorfismo

--- ExamenParcialIIEstacionamiento/src/Estacionamiento.java
+++ ExamenParcialIIEstacionamiento/src/Estacionamiento.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Estacionamiento {
+
+    private ArrayList<Vehiculo> vehiculos;
+    private HashSet<String> placas;
+
+    public Estacionamiento() {
+
+        vehiculos = new ArrayList<>();
+        placas = new HashSet<>();
+    }
 }
